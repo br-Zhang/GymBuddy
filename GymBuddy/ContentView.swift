98ead69a0cf04d406b87d7e386d6de7f19dd71d2
Brendan Zhang <brenzhan@amazon.com>
2020-01-19 14:37:05 -0800

    Initial Commit

---
+++ GymBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymBuddy
+//
+//  Created by Zhang, Brendan on 2020-01-19.
+//  Copyright © 2020 gymbuddy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
